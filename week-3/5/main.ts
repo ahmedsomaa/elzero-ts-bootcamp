1e9143327ea586fee3f4898326d464b84062bf7e
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-24 15:11:17 +0200

    feat: add week 3 asgmt 5 soln

---
+++ week-3/5/main.ts
@@ -0,0 +1,12 @@
+class Shorten {
+  constructor(public id: number, public username: string, protected title: string) {
+    this.id = id;
+    this.username = username;
+    this.title = title;
+  }
+}
+
+let tester = new Shorten(100, 'Elzero', 'Developer');
+
+console.log(tester.id);
+console.log(tester.username);
